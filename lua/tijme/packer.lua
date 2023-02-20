0adc15939f33d63221a12050e8914afb93761586
7ijme <tijmevh@gmail.com>
2023-02-20 15:02:45 +0100

    wow this is cool thanks

--- lua/tijme/packer.lua
+++ lua/tijme/packer.lua
@@ -10,7 +10,7 @@ return require('packer').startup(function(use)
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
 	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
+	  requires = { {'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-project.nvim', 'nvim-telescope/telescope-file-browser.nvim'} }
   }
 
   use('tyrannicaltoucan/vim-deep-space')
