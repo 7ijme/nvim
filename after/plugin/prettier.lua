fa147b544dda9adb1b86120bf51049679ec1a9c6
7ijme <tijmevh@gmail.com>
2023-02-20 16:08:19 +0100

    prettier remap

--- after/plugin/prettier.lua
+++ after/plugin/prettier.lua
@@ -1,3 +1,7 @@
 vim.api.nvim_exec([[
   autocmd BufWritePre *.js,*.jsx,*.ts,*.tsx,*.html,*.css,*scss Prettier
 ]], false)
+
+vim.keymap.set ("n", "<leader>pr", ":Prettier<CR>" )
+--vim.keymap.set ("n", "<leader>p", "<Nop>" )
+vim.cmd('unmap <leader>p')
