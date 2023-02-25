670acb87cee4ebbcdf754f0c17e13fcb9c270ee7
7ijme <tijmevh@gmail.com>
2023-02-25 17:21:40 +0100

    Added trouble plugin

--- lua/tijme/packer.lua
+++ lua/tijme/packer.lua
@@ -91,4 +91,16 @@ use 'nvim-lualine/lualine.nvim'
 
 --use "Pocco81/auto-save.nvim"
 
+use {
+  "folke/trouble.nvim",
+  requires = "nvim-tree/nvim-web-devicons",
+  config = function()
+    require("trouble").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  end
+}
+
 end)
