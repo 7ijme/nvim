81c56afffca69f3ff3f838313736e67b0c8fb670
Tijme <tijmevh@gmail.com>
2024-01-03 16:35:08 +0100

    fix: some small changes, added cloack, fixed git pull

--- lua/tijme/plugins/all.lua
+++ lua/tijme/plugins/all.lua
@@ -5,7 +5,7 @@ return {
 		build = ":TSUpdate",
 		config = function()
 			require("nvim-treesitter.configs").setup({
-				ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "rust", "markdown", "markdown_inline" },
+				ensure_installed = { "lua", "vim", "vimdoc", "rust"},
 				sync_install = true,
 				auto_install = true,
 				highlight = {
@@ -65,9 +65,12 @@ return {
 	-- error tab
 	{
 		"folke/trouble.nvim",
-		dependencies = "nvim-tree/nvim-web-devicons",
+		dependencies = {"nvim-tree/nvim-web-devicons"},
 		config = function()
 			require("trouble").setup({})
+			vim.keymap.set("n", "<leader>tt", function ()
+				require("trouble").toggle()
+			end)
 		end,
 	},
 
@@ -158,4 +161,35 @@ return {
 	},
 
 	"vimpostor/vim-tpipeline",
+	{"laytan/cloak.nvim",
+		config = function ()
+			require('cloak').setup({
+				enabled = true,
+				cloak_character = '*',
+				-- The applied highlight group (colors) on the cloaking, see `:h highlight`.
+				highlight_group = 'Comment',
+				-- Applies the length of the replacement characters for all matched
+				-- patterns, defaults to the length of the matched pattern.
+				cloak_length = nil, -- Provide a number if you want to hide the true length of the value.
+				-- Wether it should try every pattern to find the best fit or stop after the first.
+				try_all_patterns = true,
+				patterns = {
+					{
+						-- Match any file starting with '.env'.
+						-- This can be a table to match multiple file patterns.
+						file_pattern = '.env*',
+						-- Match an equals sign and any character after it.
+						-- This can also be a table of patterns to cloak,
+						-- example: cloak_pattern = { ':.+', '-.+' } for yaml files.
+						cloak_pattern = '=.+',
+						-- A function, table or string to generate the replacement.
+						-- The actual replacement will contain the 'cloak_character'
+						-- where it doesn't cover the original text.
+						-- If left emtpy the legacy behavior of keeping the first character is retained.
+						replace = nil,
+					},
+				},
+			})
+		end
+	}
 }
