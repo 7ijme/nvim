a840459fc7e0923c7db8e17f30a6801f40c6077d
Tijme <tijmevh@gmail.com>
2024-01-08 12:49:02 +0100

    feature: Alt-e in insert mode for auto closing

--- lua/tijme/plugins/autopair.lua
+++ lua/tijme/plugins/autopair.lua
@@ -1,5 +1,24 @@
 return {
     'windwp/nvim-autopairs',
     event = "InsertEnter",
-    opts = {} -- this is equalent to setup({}) function
+    opts = {}, -- this is equalent to setup({}) function
+	config = function()
+		local npairs = require("nvim-autopairs")
+
+		npairs.setup({
+			fast_wrap = {
+				map = '<M-e>',
+				chars = { '{', '[', '(', '"', "'" },
+				pattern = [=[[%'%"%>%]%)%}%,]]=],
+				end_key = '$',
+				before_key = 'h',
+				after_key = 'l',
+				cursor_pos_before = true,
+				keys = 'qwertyuiopzxcvbnmasdfghjkl',
+				manual_position = true,
+				highlight = 'Search',
+				highlight_grey='Comment'
+			},
+		})
+	end
 }
