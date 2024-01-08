1c99bb7eeaf480588f26815d9d446e9d4606ccc1
Tijme <tijmevh@gmail.com>
2024-01-08 00:25:27 +0000

    Added gj in visual mode

--- lua/tijme/remap.lua
+++ lua/tijme/remap.lua
@@ -22,6 +22,9 @@ vim.keymap.set("n", "<A-Down>", "<cmd>resize +1<CR>") ]]
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
+-- join lines
+vim.keymap.set("v", "gj", "J")
+-- move lines up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
