5b66c542d1e50fb9b53a7de7f0a1a1b3bb0271be
Tijme <tijmevh@gmail.com>
2024-01-14 12:58:47 +0100

    Enable git blame

--- lua/tijme/plugins/all.lua
+++ lua/tijme/plugins/all.lua
@@ -27,7 +27,9 @@ return {
 	{
 		"lewis6991/gitsigns.nvim",
 		config = function()
-			require("gitsigns").setup()
+			require("gitsigns").setup({
+				current_line_blame = true
+			})
 		end,
 	},
 
