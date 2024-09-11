a419b94a24ccc295db2ee2c307dc5ee9a1554865
Tijme <tijmevh@gmail.com>
2024-09-11 10:41:16 +0000

    fixed bash

--- lua/tijme/plugins/noice.lua
+++ lua/tijme/plugins/noice.lua
@@ -27,7 +27,7 @@ return {
 					cmdline = { pattern = "^:", icon = "", lang = "vim" },
 					search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
 					search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
-					filter = { pattern = "^:%s!*", icon = "$", lang = "bash" },
+					filter = { pattern = "^:!", icon = "$", lang = "bash" },
 					lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
 					help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
 					input = {}, -- Used by input()
